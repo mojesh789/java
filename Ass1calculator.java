7f7c118a8afaa8aaa85a607ef654dd9924334b5d
mojesh789 <151903227+mojesh789@users.noreply.github.com>
2024-10-16 11:18:25 +0530

    Update Ass1calculator.java

--- Ass1calculator.java
+++ Ass1calculator.java
@@ -20,7 +20,7 @@ case '+':System.out.println("Addition "+(a+b));
 break;
 case '-':System.out.println("Substraction: "+(a-b));
 break;
-case '':System.out.println("Multiplication:"+(ab));
+case '*':System.out.println("Multiplication:"+(a*b));
 break;
 case '/':System.out.println("Division: "+(a/b));
 break;
